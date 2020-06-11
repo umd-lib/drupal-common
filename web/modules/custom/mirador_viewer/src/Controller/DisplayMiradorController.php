4b95b237dcc88115e0158e1b83c2b13215247f20
Jeremy M. Gottwig <jgottwig@umd.edu>
2020-06-11 17:28:31 -0400

    LIBWHCA-4. Porting viewer code/hash parser into Drupal from Hippo.
    https://issues.umd.edu/browse/LIBWHCA-4

--- web/modules/custom/mirador_viewer/src/Controller/DisplayMiradorController.php
+++ web/modules/custom/mirador_viewer/src/Controller/DisplayMiradorController.php
@@ -5,18 +5,69 @@ namespace Drupal\mirador_viewer\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Security\TrustedCallbackInterface;
 
-class DisplayMiradorController extends ControllerBase {
+class DisplayMiradorController extends ControllerBase implements TrustedCallbackInterface {
 
-  public function ViewObject($object_id) {
-    $config = \Drupal::config('mirador_viewer.adminsettings');
+  private $config;
 
+  public function __construct(AccountInterface $current_user) {
+    $this->config = \Drupal::config('mirador_viewer.adminsettings');
+  }
+
+  public function viewObject($object_id) {
     return [
       '#theme' => 'mirador_viewer',
       '#title' => $this->t('test object'),
-      '#iiif_server' => $config->get('iiif_server'),
-      '#error_message' => $config->get('error_message'),
+      '#iiif_server' => $this->config->get('iiif_server'),
+      '#error_message' => $this->config->get('error_message'),
       '#object_id' => $object_id,
     ];
+  }
+
+  /**
+   * Returns a pair tree prefixed hash id.
+   *
+   * @param hashId
+   *          a fedora object hash id (example:
+   *          6bdc4c4e-f937-4c37-92cc-daf42a5cd4c5)
+   * @return prefixedHashId a pair tree prefixed hash id (example:
+   *         6b/dc/4c/4e/6bdc4c4e-f937-4c37-92cc-daf42a5cd4c5)
+   */
+  public static addPairTreePrefix($hash) {
+    $hash_split = explode('-', $hash);
+    $tree_length = 2;
+    $prefix_split = str_split($hash_split[0], $tree_length);
+    return implode("/", $split) . '/' . $hash;
+  }
+
+  /*
+   * Generate new Document ID if the document belongs to Fedora Database
+   *
+   * @param HstRequestContext
+   *
+   * @param Properties
+   *
+   * @param String
+   *
+   * @return String
+   */
+  private static String generateFedoraDatabaseDocumentID($id) {
+    $fc_base = $this->config->get('fcrepo_server');
+    $pcdm_prefix = "pcdm/";
+
+    if ($fc_base == NULL) {
+      // throw exception
+    }
+
+    $pcdm_path = $pcdm_prefix . '/' . $this->addPairTreePrefix($id);    
+
+    return $fc_base . $pcdm_path;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function trustedCallbacks() {
+    return ['generateFedoraDatabaseDocumentID', 'addPairTreePrefix', 'viewObject'];
   }
 
 }
--- web/modules/custom/mirador_viewer/src/Form/SettingsForm.php
+++ web/modules/custom/mirador_viewer/src/Form/SettingsForm.php
@@ -37,7 +37,20 @@ class SettingsForm extends ConfigFormBase {
     $form['iiif_server'] = [  
       '#type' => 'textfield',  
       '#title' => $this->t('IIIF Server'),  
-      '#default_value' => $config->get('iiif_server'),  
+      '#default_value' => $config->get('iiif_server'),
+      '#description' => $this->t('E.g., https://iiifdev.lib.umd.edu/ with trailing slash.'),
+    ];
+    $form['iiif_viewer'] = [
+      '#type' => 'textfield',  
+      '#title' => $this->t('IIIF Viewer Path'),  
+      '#default_value' => $config->get('iiif_viewer'),
+      '#description' => $this->t('E.g., viewer/1.2.0/mirador.html'),
+    ];
+    $form['fcrepo_server'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Fcrepo Server'),
+      '#default_value' => $config->get('fcrepo_server'),
+      '#description' => $this->t('E.g., https://fcrepodev.lib.umd.edu/fcrepo/rest/ with trailing slash.'),
     ];
     $form['error_message'] = [
       '#type' => 'textfield',
@@ -56,6 +69,14 @@ class SettingsForm extends ConfigFormBase {
 
     $this->config('mirador_viewer.adminsettings')  
       ->set('iiif_server', $form_state->getValue('iiif_server'))  
+      ->save();
+
+    $this->config('mirador_viewer.adminsettings')  
+      ->set('iiif_viewer', $form_state->getValue('iiif_viewer'))  
+      ->save();
+
+    $this->config('mirador_viewer.adminsettings')  
+      ->set('fcrepo_server', $form_state->getValue('fcrepo_server'))  
       ->save();
 
     $this->config('mirador_viewer.adminsettings')
