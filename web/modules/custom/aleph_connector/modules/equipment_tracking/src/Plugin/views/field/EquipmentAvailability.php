f771436ef463ecd76aef0d4f88f76dc439c17e5f
David P. Steelman <dsteelma@umd.edu>
2021-10-01 14:32:47 -0400

    LIBWEB-5449. Added logging to EquipmentAvailability using dep. injection
    Added logging to EquipmentAvailability using dependency injection,
    and modified exception handling to generate errors in the log.
    
    https://issues.umd.edu/browse/LIBWEB-5449

--- web/modules/custom/aleph_connector/modules/equipment_tracking/src/Plugin/views/field/EquipmentAvailability.php
+++ web/modules/custom/aleph_connector/modules/equipment_tracking/src/Plugin/views/field/EquipmentAvailability.php
@@ -12,6 +12,8 @@ use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 use Drupal\aleph_connector\Controller\AlephController;
 use Drupal\Component\Datetime\DateTimePlus;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Field handler to flag the node type.
@@ -21,6 +23,45 @@ use Drupal\Component\Datetime\DateTimePlus;
  * @ViewsField("aleph_equipment_available")
  */
 class EquipmentAvailability extends FieldPluginBase {
+
+  /**
+   * The logger instance.
+   *
+   * @var Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a PluginBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+   *   The logger instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->definition = $plugin_definition + $configuration;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      // Inject a logger instance.
+      $container->get('logger.factory')->get('equipment_availability')
+    );
+  }
 
   /**
    * @{inheritdoc}
@@ -84,28 +125,26 @@ class EquipmentAvailability extends FieldPluginBase {
    */
   public function availabilityField($bibnums, $availability_data) {
     $available_count = 0;
-
     $earliest_raw_date = null;
     $processed_date = NULL;
 
     foreach ($availability_data as $datum) {
       $available_count += $datum['available'];
 
-      if ($raw_date = $datum['mindue']) {
+      if ($raw_date = ($datum['mindue'])) {
         if (!$earliest_raw_date || ($raw_date < $earliest_raw_date)) {
           $earliest_raw_date = $raw_date;
           try {
-            // 2021-07-30-16:30
+            // Expected raw_data format: 202107301630
+            // (i.e. 16:30 on July 30, 2021)
             $datetime_p = DateTimePlus::createFromFormat('YmdHi', $raw_date);
             $processed_date = $datetime_p->format('g:i A \o\n F j, Y');
           }
           catch (\InvalidArgumentException $e) {
-            // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 1: ' . $e->getMessage());
-            dpm($e);
+            $this->logger->error("InvalidArgumentException: Error parsing date: '$raw_date'.");
           }
           catch (\UnexpectedValueException $e) {
-            // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 2: ' . $e->getMessage());
-            dpm($e);
+            $this->logger->error("UnexpectedValueException: Error parsing date: '$raw_date'.", $e);
           }
         }
       }
