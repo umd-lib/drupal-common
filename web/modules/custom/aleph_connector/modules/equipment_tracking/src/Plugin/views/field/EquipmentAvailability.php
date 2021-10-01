54a683d828fae0266d52f0a58e3c0dbd5ddab7c6
David P. Steelman <dsteelma@umd.edu>
2021-10-01 13:48:25 -0400

    LIBWEB-5449. Updated EquipmentAvailability to handle multiple bibnums
    Updated EquipmentAvailability to handle equipment records with
    multiple bibnums by summing up the individual "available" counts for
    the bibnums, and using the earliest "mindue" field (if present) for the
    earliest due date.
    
    https://issues.umd.edu/browse/LIBWEB-5449

--- web/modules/custom/aleph_connector/modules/equipment_tracking/src/Plugin/views/field/EquipmentAvailability.php
+++ web/modules/custom/aleph_connector/modules/equipment_tracking/src/Plugin/views/field/EquipmentAvailability.php
@@ -8,7 +8,6 @@
 namespace Drupal\equipment_tracking\Plugin\views\field;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\node\Entity\NodeType;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 use Drupal\aleph_connector\Controller\AlephController;
@@ -60,41 +59,61 @@ class EquipmentAvailability extends FieldPluginBase {
   public function render(ResultRow $values) {
     $bibnums = [];
     $aleph = new AlephController();
+
     if (!$this->isValueEmpty($aleph->getQueryField(), TRUE)) {
-      if ($bibnum = $this->view->field[$aleph->getQueryField()]->original_value->__toString()) {
-        $bibnums[] = $bibnum;
+      $field = $this->view->field[$aleph->getQueryField()];
+      if ($field instanceof \Drupal\search_api\Plugin\views\field\SearchApiEntityField) {
+        $items = $field->getItems($values);
+        foreach ($items as $item) {
+          $bibnum = $item['value'];
+          $bibnums[] = $bibnum;
+        }
       }
     }
+
     if (count($bibnums) > 0) {
       if ($bibnum_data = $aleph->getBibnumData($bibnums)) {
         return $this->availabilityField($bibnums, $bibnum_data);
       }
     }
-    return FALSE; 
+    return FALSE;
   }
 
   /**
    * Generate sample page content for umd-examples page.
    */
   public function availabilityField($bibnums, $availability_data) {
-    $availability_data = reset($availability_data);
-    $processed_date = null;
-    if ($raw_date = $availability_data['mindue']) {
-      try {
-        // 2021-07-30-16:30
-        $datetime_p = DateTimePlus::createFromFormat('YmdHi', $raw_date);
-        $processed_date = $datetime_p->format('g:i A \o\n F j, Y');
-      }
-      catch (\InvalidArgumentException $e) {
-        // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 1: ' . $e->getMessage());
-      }
-      catch (\UnexpectedValueException $e) {
-        // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 2: ' . $e->getMessage());
+    $available_count = 0;
+
+    $earliest_raw_date = null;
+    $processed_date = NULL;
+
+    foreach ($availability_data as $datum) {
+      $available_count += $datum['available'];
+
+      if ($raw_date = $datum['mindue']) {
+        if (!$earliest_raw_date || ($raw_date < $earliest_raw_date)) {
+          $earliest_raw_date = $raw_date;
+          try {
+            // 2021-07-30-16:30
+            $datetime_p = DateTimePlus::createFromFormat('YmdHi', $raw_date);
+            $processed_date = $datetime_p->format('g:i A \o\n F j, Y');
+          }
+          catch (\InvalidArgumentException $e) {
+            // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 1: ' . $e->getMessage());
+            dpm($e);
+          }
+          catch (\UnexpectedValueException $e) {
+            // TODO: Replace this with logger -- \Drupal::messenger()->addError('Error 2: ' . $e->getMessage());
+            dpm($e);
+          }
+        }
       }
     }
+
     return [
       '#theme' => 'aleph_equipment_available',
-      '#equipment_count' => $availability_data['available'],
+      '#equipment_count' => $available_count,
       '#equipment_mindue' => $processed_date,
       '#equipment_sysnum' => implode(',', $bibnums),
     ];
