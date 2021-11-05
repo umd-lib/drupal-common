ba34ad2ab7682d9b60daf19fab7f900e0ce77d90
Mohamed Abdul Rasheed <mohideen@umd.edu>
2021-11-05 09:22:01 -0400

    LIBWEB-5499. Systems Status Rich Content Block.
    https://issues.umd.edu/browse/LIBWEB-5499

--- web/modules/custom/system_status/js/system_status.js
+++ web/modules/custom/system_status/js/system_status.js
@@ -5,34 +5,40 @@
     attach: function (context, settings) {
       /**
        * Retrieves the systems status from the given endpoint, and updates
-       * the "Systems Status" entry in the utility navigation menu
+       * the "Systems Status" entry in the utility navigation menu, and the
+       * system status block.
        */
       function retrieveStatus(systemStatusUrl) {
         var utilityNavItem = $(context).find('.utility-nav-systems-status');
+        var systemStatusDate = $(context).find('.system-status-date');
+        var systemStatusOperational = $(context).find('.system-status-operational > .status');
+        var systemStatusProblem = $(context).find('.system-status-maintenance > .status');
+        var systemStatusOutage = $(context).find('.system-status-outage > .status');
 
         if (utilityNavItem === undefined || utilityNavItem[0] === undefined) {
           // Status menu item not on page
           return;
         }
 
-        $.getJSON( systemStatusUrl, function( data ) {
+        $.getJSON(systemStatusUrl, function (data) {
           if (data === undefined) {
             return;
           }
 
           var nonNormalCount = data['non_normal']
-          if (nonNormalCount === undefined) {
-            return;
+          if (nonNormalCount > 0) {
+            var currentCaption = utilityNavItem[0].textContent
+            var nonNormalCaption = '<span class="badge">' + nonNormalCount + '</span>';
+            utilityNavItem.html(currentCaption + " " + nonNormalCaption);
           }
 
-          if (nonNormalCount == 0) {
-            // No further action if all systems are operational
-            return;
-          }
-
-          var currentCaption = utilityNavItem[0].textContent
-          var nonNormalCaption = '<span class="badge">'+nonNormalCount+'</span>';
-          utilityNavItem.html(currentCaption + " " + nonNormalCaption);
+          systemStatusDate.html(getFormattedDate());
+          systemStatusOperational.html(`${data['normal']}/${data['total']}`);
+          var problemHtml = "<ul>"
+          data['non_normal_list'].forEach(element => problemHtml += `<li>${element}</li>`);
+          problemHtml += "</ul>"
+          systemStatusProblem.html(problemHtml);
+          systemStatusOutage.html(`${data['outage']}/${data['total']}`);
         });
       }
 
@@ -40,6 +46,17 @@
         var systemStatusUrl = drupalSettings.system_status.system_status_url;
         retrieveStatus(systemStatusUrl);
       });
+
+      function getFormattedDate(date) {
+        if (date == undefined) {
+          date = new Date();
+        }
+        let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
+        let da = new Intl.DateTimeFormat('en', { day: 'numeric' }).format(date);
+        let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+        let time = new Intl.DateTimeFormat('en', { hour: 'numeric', minute: 'numeric' }).format(date);
+        return `${mo} ${da}, ${ye} at ${time}`;
+      }
     }
   };
 })(jQuery, Drupal, drupalSettings);
