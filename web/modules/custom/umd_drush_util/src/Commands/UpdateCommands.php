9e58d37f933724754e63a3c1c93ad1a42874432b
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2023-10-26 11:21:44 -0400

    LIBCAB-1527. New drush utils module for occasionally needed tools.
    https://umd-dit.atlassian.net/browse/LIBCAB-1527

---
+++ web/modules/custom/umd_drush_util/src/Commands/UpdateCommands.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\umd_drush_util\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
+/**
+ * A Drush commandfile.
+ *
+ * @see
+ *   - https://www.drupal.org/project/drupal/issues/2916266
+ */
+class UpdateCommands extends DrushCommands {
+
+  /**
+   * The key value store to use.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
+   */
+  protected $keyValueStore;
+
+  /**
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $key_value_factory
+   *   The key value store to use.
+   */
+  public function __construct(KeyValueFactoryInterface $key_value_factory) {
+    $this->keyValueStore = $key_value_factory;
+  }
+
+  /**
+   * Corrects a field storage configuration. See https://www.drupal.org/project/drupal/issues/2916266 for more info
+   *
+   * @command update:correct-field-config-storage
+   *
+   * @param string $entity_type
+   *   Entity type
+   * @param string $bundle
+   *   Bundle name
+   * @param string $field_name
+   *   Field name
+   */
+  public function correctFieldStorageConfig($entity_type, $bundle, $field_name) {
+    $field_map_kv_store = $this->keyValueStore->get('entity.definitions.bundle_field_map');
+    $map = $field_map_kv_store->get($entity_type);
+    unset($map[$field_name]['bundles'][$bundle]);
+    $field_map_kv_store->set($entity_type, $map);
+  }
+}
