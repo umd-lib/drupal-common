66038e866c6b205c5c16c481b2e86b11300cfbe9
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2024-06-11 07:49:21 -0400

    LIBWEB-6079. Javascript efficiency improvements for hero form.
    https://umd-dit.atlassian.net/browse/LIBWEB-6079

--- web/modules/custom/hero_search/js/form_util.js
+++ web/modules/custom/hero_search/js/form_util.js
@@ -1,24 +1,20 @@
 // Update drop-downs with query string
 
 window.onload = function() {
-  document.getElementById("search-query-input-all").addEventListener('change', updateSearchers);
-  const articles_dd = document.getElementById("search-dropdown-articles");
-  const books_dd = document.getElementById("search-dropdown-books");
-  const journals_dd = document.getElementById("search-dropdown-journals");
-  const articles_base = articles_dd.href;
-  const books_base = books_dd.href;
-  const journals_base = journals_dd.href;
+  const searchInput = document.getElementById("search-query-input-all");
+  const articlesDd = document.getElementById("search-dropdown-articles");
+  const booksDd = document.getElementById("search-dropdown-books");
+  const journalsDd = document.getElementById("search-dropdown-journals");
+  const articlesBase = articlesDd.href;
+  const booksBase = booksDd.href;
+  const journalsBase = journalsDd.href;
+
+  searchInput.addEventListener('input', updateSearchers);
 
   function updateSearchers() {
-    if (this.value && this.value.trim()) {
-      var query_string = "&query=any,contains," + this.value;
-      articles_dd.href = articles_base + query_string;
-      books_dd.href = books_base + query_string;
-      journals_dd.href = journals_base + query_string;
-    } else {
-      articles_dd.href = articles_base;
-      books_dd.href = books_base;
-      journals_dd.href = journals_base;
-    }
+    const queryString = this.value.trim() ? "&query=any,contains," + this.value.trim() : "";
+    articlesDd.href = articlesBase + queryString;
+    booksDd.href = booksBase + queryString;
+    journalsDd.href = journalsBase + queryString;
   }
 }
