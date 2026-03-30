f9998cb07fbe948ac51add3e4341e75c27d3fe31
Jeremy M Gottwig <jgottwig@umd.edu>
2026-03-30 08:27:09 -0400

    LIBWEB-6662. WHPool detail page changes for solrizer index.
    https://umd-dit.atlassian.net/browse/LIBWEB-6662

--- web/modules/custom/mirador_viewer/src/EventSubscriber/SolrQueryAlterEventSubscriber.php
+++ web/modules/custom/mirador_viewer/src/EventSubscriber/SolrQueryAlterEventSubscriber.php
@@ -42,6 +42,49 @@ class SolrQueryAlterEventSubscriber implements EventSubscriberInterface {
     foreach ($result_items as $key => $item) {
       $extra = $item->getExtraData('search_api_solr_document');
       if (!empty($extra)) {
+        $members = $extra->__get('object__has_member');
+        $body = NULL;
+        $attachments = [];
+        if (!empty($members)) {
+          foreach ($members as $member) {
+            $type = !empty($member['page__title__txt']) ? $member['page__title__txt'] : NULL;
+            $files = !empty($member['page__has_file']) ? $member['page__has_file'] : [];
+            if (empty($type) || empty($files)) {
+              continue;
+            }
+            foreach ($files as $file) {
+              $mime_type = !empty($file['file__mime_type__str']) ? $file['file__mime_type__str'] : NULL;
+              if ($type == 'Body' && !empty($mime_type) && $mime_type == 'text/html') {
+                $body_url = !empty($file['id']) ? $file['id'] : NULL;
+                if (!empty($body_url)) {
+                  $new_url = preg_replace(
+                    '#^https?://[^/]+#',
+                    'http://fcrepo-webapp:8080',
+                    $body_url
+                  );
+                  $ch = curl_init();
+                  curl_setopt($ch, CURLOPT_URL, $new_url);
+                  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                  $body = curl_exec($ch);
+                  curl_close($ch);
+                }
+              } elseif (str_contains($type, "Attachment") && !empty($file['id'])) {
+                if (!empty($mime_type) && $mime_type == 'application/pdf') {
+                  $attachments[] = "<a href='" . $file['id'] . "' >PDF Attachment</a>";
+                } else {
+                  $attachments[] = "<img src='" . $file['id'] . "' />";
+                }
+              }
+            }
+          }
+
+          if (!empty($body)) {
+            $item->setExcerpt($body . implode(" ", $attachments));
+          }
+          return;
+        }
+
+        // Legacy Support -- Remove post release
         $files = $extra->__get('files');
         if (!empty($files['docs'])) {
           foreach ($files['docs'] as $f_doc) {
@@ -65,12 +108,34 @@ class SolrQueryAlterEventSubscriber implements EventSubscriberInterface {
   public function preQuery(PreQueryEvent $event): void {
     $search_query = $event->getSearchApiQuery();
     $search_index = $search_query->getIndex();
-    if ($search_index) {
-      $index_id = $search_index->id();
-      if ($index_id != 'fcrepo') {
+    if (empty($search_index)) {
+      return;
+    }
+    $index_id = $search_index->id();
+
+    if ($index_id != 'fcrepo') {
+      $server_id = $search_index->getServerId();
+      if ($server_id != "fcrepo") {
         return;
       }
-    }
+
+      $query = $event->getSolariumQuery();
+      if (empty($query)) {
+        return;
+      }
+      $query->addField('[child]');
+      $query->addField('object__has_member');
+      $query->addField('page__has_file');
+      $query->addField('page__title__txt');
+      $query->addField('id');
+      $query->addField('file__mime_type__str');
+      return;
+    } 
+
+
+
+
+    // Legacy Support -- Remove post release
     $query = $event->getSolariumQuery();
     $query->addField('annotation_source_type:[subquery]');
     $query->addField('files:[subquery]');
